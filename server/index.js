8846ced1776b78f1f81906c48679af266f894cba
James Sheehan <jmsdevx@gmail.com>
2019-01-23 16:52:43 -0600

    add basic get endpoints

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const db = require("../db/db");
-const { json, urlencoded } = require("body-parser");
+const { json } = require("body-parser");
 
 const app = express();
 app.use(json());
@@ -9,7 +9,39 @@ app.get("/api/test", (req, res) => {
   res.status(200).send({
     success: "true",
     message: "test retrieved",
-    test: db
+    test: db.test
+  });
+});
+
+app.get("/api/types", (req, res) => {
+  res.status(200).send({
+    success: "true",
+    message: "types retrieved",
+    types: db.types
+  });
+});
+
+app.get("/api/types/name/:name", (req, res) => {
+  const name = req.params.name.toLowerCase();
+  const nameQuery = db.types.filter((e, i) => {
+    return e.name == name;
+  });
+  res.status(200).send({
+    success: "true",
+    message: `archetype of ${name} retrieved`,
+    types: nameQuery
+  });
+});
+
+app.get("/api/types/alias/:alias", (req, res) => {
+  const alias = req.params.alias.toLowerCase();
+  const aliasQuery = db.types.filter((e, i) => {
+    return e.alias == alias;
+  });
+  res.status(200).send({
+    success: "true",
+    message: `archetype of ${alias} retrieved`,
+    types: aliasQuery
   });
 });
 
