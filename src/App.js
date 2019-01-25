c44efd445ff1f2426ee2031ab5fed57e64421aca
James Sheehan <jmsdevx@gmail.com>
2019-01-25 16:06:08 -0600

    add image loader

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Container from "./components/layout/Container";
 import Nav from "./components/layout/Nav";
 import ReactDOM from "react-dom";
 import "./styles/App.css";
+import faces from "./images/faces.PNG";
 
 class App extends Component {
   render() {
@@ -10,6 +11,7 @@ class App extends Component {
       <div>
         <Nav />
         <Container />
+        <img src={faces} alt="faces" />
       </div>
     );
   }
